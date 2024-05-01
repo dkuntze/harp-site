5b6de24414ae88bfc3e5aa8ad16b4e63c0b31e52
Darin Kuntze <dkuntze@gmail.com>
2024-05-01 14:41:39 -0500

    detect paragraphs

--- blocks/available-bird/available-bird.js
+++ blocks/available-bird/available-bird.js
@@ -12,9 +12,18 @@ export default async function decorate(block) {
   const copyDiv = document.createElement('div');
   const h2 = document.createElement('h2');
   const copyP = document.createElement('p');
-  copyP.innerHTML = config.text;
   h2.innerHTML = config.name;
   copyDiv.appendChild(h2);
+  // this detects if its an array of paragraphs or just a single paragraph
+  if (Array.isArray(config.text)) {
+    config.text.forEach((text) => {
+      const p = document.createElement('p');
+      p.innerHTML = text;
+      copyDiv.appendChild(p);
+    });
+  } else {
+    copyP.innerHTML = config.text;
+  }
   copyDiv.classList.add('copy');
   copyDiv.append(copyP);
   const image = document.createElement('img');
